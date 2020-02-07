efb64f40d0a292fae61fc4e48a98a7e28bba4f3e
Roberto Morais <fuenrob@gmail.com>
2020-02-07 17:16:51 +0100

    Use production URI mongo like a server prod variable

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 /**
  * PORT
  */
-
 process.env.PORT = process.env.PORT || 3000;
 
 /**
@@ -9,16 +8,14 @@ process.env.PORT = process.env.PORT || 3000;
  */
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-
 /**
  * BBDD
  */
-
 let urlServerBBDD;
 if(process.env.NODE_ENV === 'dev'){
     urlServerBBDD = 'mongodb://localhost:27017/intranet';
 }else{
-    urlServerBBDD = 'mongodb+srv://<user>:<password>@cluster0-nixve.mongodb.net/intranet';
+    urlServerBBDD = process.env.MONGO_URI;
 }
 
 process.env.urlServerBBDD = urlServerBBDD;
